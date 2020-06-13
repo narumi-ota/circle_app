4a4c7bbfecd3bbac2a91ae7e933a10cadbb22e5b
narumi <otanarumi930@gmail.com>
2020-06-13 16:35:47 +0900

    add design for user index page

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,16 +4,24 @@
 
 @section('content')
 
-<h1>Users</h1>
-    <ul>
-        @forelse ($users as $user)
-        <li>
-          <a>{{ $user->name }}</a>
-          <a href="{{ action('UsersController@show',$user) }}">{{ $user->name }}</a>
-        </li>
-        @empty
-        <ll>No Users yet</li>
-        @endforelse
-    </ul>
+<h1>すべてのユーザー</h1>
+    <table>
+        <tbody>
+        @foreach ($users as $user)
+            <tr>
+                <td>
+                    <a href="{{ action('UsersController@show',$user) }}">
+                    <img src="{{ $user->image_path }}" class="comment_user_icon" alt="user_icon">
+                    <p>{{ $user->name }}</p>
+                </td>
+                <td>
+                    <div class="speech_bubble_left">
+                        <p>{{ $user->message }}</P>
+                    </div>
+                </td>
+            <tr>
+        @endforeach
+        </tbody>
+    </table>
 
 @endsection
