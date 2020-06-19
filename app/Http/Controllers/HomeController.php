fb639a662c86b68efdd72571bb304856fe57d4dd
narumi <otanarumi930@gmail.com>
2020-06-19 14:19:46 +0900

    modify store function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -50,6 +50,7 @@ class HomeController extends Controller
         $image = $request->file('image');
         $path = Storage::disk('s3')->putFile('myprefix', $image, 'public');
         $user->image_path = Storage::disk('s3')->url($path);
+        $user->save();
         return redirect()->back();
     }
 
